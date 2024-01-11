b8d9a360e1ad50978b7170de60b0321c16a9c203
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-01-11 18:35:06 -0400

    Autenticação do usuário.

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -6,7 +6,7 @@ class TagsController{
 
         const tags = await knex("tags").where({ user_id })
         
-        res.json(tags)
+        return res.json(tags)
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ const Router = require("express");
 const usersRoutes = require("./users.routes");
 const notesRoutes = require("./notes.routes");
 const tagsRoutes = require("./tags.routes");
-
+const sessionRoutes = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes)
 routes.use("/notes", notesRoutes)
 routes.use("/tags", tagsRoutes)
+routes.use("/sessions", sessionRoutes)
 
 
 module.exports = routes;
