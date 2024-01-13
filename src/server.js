932a9d3ea238007507ec8ff919471fc205090840
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-01-13 19:02:30 -0400

    Integrando o frontend com o backend

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const routes = require("./routes")
 const AppError = require("./utils/AppError")
 const migrationsRun = require("./database/sqlite/migrations")
 const uploadConfig = require("./configs/upload")
+const cors = require("cors")
 migrationsRun();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 // Rota para devolver o avatar para o usuário
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "htpp://localhost:3333"
+
+})
